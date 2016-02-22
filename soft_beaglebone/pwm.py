faeca2281ca94747f1aed805d70ea3dce6b38894
Anthony Arthus <anthony.arthus@gmail.com>
2016-02-22 23:18:31 +0100

    Ajout architecture soft

---
+++ soft_beaglebone/pwm.py
@@ -0,0 +1,15 @@
+class PWM:
+    def __init__(self):
+        pass
+        
+    def set_duty(self, channel, duty)
+        pass
+    
+    def set_frequency(self, channel, frequency)
+        pass
+    
+    def start(self, channel, duty)
+        pass
+    
+    def start(self, channel, duty, frequency)
+        pass
