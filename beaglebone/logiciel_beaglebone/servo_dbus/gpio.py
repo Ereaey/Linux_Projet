d2027d6257eb740c9017ad8abc0fa19ef9880325
Anthony Arthus <anthony.arthus@gmail.com>
2016-03-17 18:46:42 +0100

    Permet de recuperer la valeur de l'angle du servo

--- beaglebone/logiciel_beaglebone/servo_dbus/gpio.py
+++ beaglebone/logiciel_beaglebone/servo_dbus/gpio.py
@@ -7,7 +7,7 @@ class GPIO:
     
     def create(self, pin):
         os.system("echo "+str(pin)+" > /sys/class/gpio/export")
-        time.sleep(3)
+        time.sleep(2)
     
     def inputPin(self, pin):
         os.system("echo in > /sys/class/gpio/gpio"+str(pin)+"/direction");
--- beaglebone/logiciel_beaglebone/servo_dbus/pwm.py
+++ beaglebone/logiciel_beaglebone/servo_dbus/pwm.py
@@ -9,11 +9,17 @@ def writeFile(file, data):
     f = open(file, 'w')
     f.write(data)
     f.close()
+
+def readFile(file):
+    f = open(file, 'r')
+    data = f.read()
+    f.close()
+    return data
     
 class PWM:
     def __init__(self, channel, duty, period):
         os.system("echo bone_pwm_"+channel+" > /sys/devices/bone_capemgr.8/slots")
-        time.sleep(3)
+        time.sleep(2)
         self.name = self.getNameChannel(channel)
         os.system("echo " + str(period) + " > /sys/devices/ocp.3/"+self.name+"/period")
         os.system("echo " + str(duty) + " > /sys/devices/ocp.3/"+self.name+"/duty")
@@ -28,6 +34,12 @@ class PWM:
 
     def set_polarity(self, polarity):
         writeFile("/sys/devices/ocp.3/"+self.name+"/polarity", str(polarity))
+        
+    def get_duty(self):
+        return readFile("/sys/devices/ocp.3/"+self.name+"/duty")
+        
+    def get_period(self):
+        return readFile("/sys/devices/ocp.3/"+self.name+"/period")
 
     def getNameChannel(self, channel):
         name = os.popen("ls /sys/devices/ocp.3 | grep pwm_test_"+channel)
