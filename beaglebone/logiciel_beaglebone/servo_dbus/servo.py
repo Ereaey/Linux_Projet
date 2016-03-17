6f6f886cbe2a8f92519c01024650aa7badc4abb7
Anthony Arthus <anthony.arthus@gmail.com>
2016-03-17 05:56:11 +0100

    Modification de la formule, modification de l'init

--- beaglebone/logiciel_beaglebone/servo_dbus/servo.py
+++ beaglebone/logiciel_beaglebone/servo_dbus/servo.py
@@ -2,14 +2,14 @@ from pwm import *
 
 class Servo:
     def __init__(self, channel):
-        self.pwm = PWM()
-        self.channel = channel
-        self.pwm.start(self.channel, 1500000, 200000000)
+        self.pwm = PWM(channel, 1500000, 200000000)
+        self.pwm.set_period(20000000)
+        self.pwm.set_duty(1500000)
 
     def setAngle(self, angle):
-        if angle >= -45 or angle <= 45:
-            duty = (angle + 45) * (1000000 / 90) + 1000000
-            self.pwm.set_duty(self.channel, duty)
+        if angle >= -90 or angle <= 90:
+            duty = (angle + 90) * (1000000 / 180) + 1000000
+            self.pwm.set_duty(duty)
 
 #s = Servo("P9_14", "P8_13")
 #s.setAngle(12)
