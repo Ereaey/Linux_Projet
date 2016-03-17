e8a4a2f4d45ea11e06d2d7fc3c04bfea9a058e82
Anthony Arthus <anthony.arthus@gmail.com>
2016-03-17 18:47:09 +0100

    Utilisation d'un repere pour dessiner

--- beaglebone/logiciel_beaglebone/servo_dbus/shape.py
+++ beaglebone/logiciel_beaglebone/servo_dbus/shape.py
@@ -4,6 +4,9 @@ import math
 class Shape:
     def __init__(self, t):
         self.t = t
+        self.zeroH = -90;
+        self.zeroV = -90;
+        #0 a 180 pour le repere
         pass
     
     def drawCircle(self):
@@ -11,25 +14,43 @@ class Shape:
         for nb in range(0, 5):
             for f in range(0, 360, 4):
                 angle = f * 2 * math.pi / 360.0
-                self.t.setAngle(int(math.cos(angle) * 25.0), int(math.sin(angle) * 25.0))
+                self.t.setAngle(int(math.cos(angle) * 15.0), int(math.sin(angle) * 15.0))
                 time.sleep(0.01)
-                
+    
+    def moveTo(self, X, Y):
+        self.t.setAngle(X + self.zeroH, Y + self.zeroV)
+        
+    def getPosition():
+        return self.t.servoHorizontal.getAngle() - self.zeroH, self.t.servoVertical.getAngle() - self.zeroV
+        
+    def drawLine(self, posX, posY):
+        print "DrawLine"
+        X = getPosition()[0]
+        Y = getPosition()[1]
+        print math.sqrt((posX - X)**2 + (posY - Y)**2)
+        pass
+        
     def drawSquare(self):
         print "Draw square"
         for nb in range(0, 5):
-            self.t.setAngle(-25, -25)
-            for i in range(-25, 26, 1):
-                self.t.setAngleVertical(i)
+            self.moveTo(75, 75)
+            #self.t.setAngle(-15, -15)
+            for i in range(75, 106, 1):
+                #self.t.setAngleVertical(i)
+                self.moveTo(75, i)
                 time.sleep(0.01)
-            for i in range(-25, 26, 1):
-                self.t.setAngleHorizontal(i)
+            for i in range(75, 105, 1):
+                #self.t.setAngleHorizontal(i)
+                self.moveTo(i, 105)
                 time.sleep(0.01)
-            for i in range(25, -26, -1):
-                self.t.setAngleVertical(i)
+            for i in range(105, 76, -1):
+                #self.t.setAngleVertical(i)
+                self.moveTo(105, i)
                 time.sleep(0.01)
-            for i in range(25, -26, -1):
-                self.t.setAngleHorizontal(i)
+            for i in range(105, 76, -1):
+                #self.t.setAngleHorizontal(i)
                 time.sleep(0.01)
+                self.moveTo(i, 75)
 
 '''
 e = Shape("dqsd")
