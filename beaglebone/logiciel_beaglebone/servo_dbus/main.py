03213c7bdac2ca1687d01a4cbfcd770ac8dae7ca
Anthony Arthus <anthony.arthus@gmail.com>
2016-03-19 01:25:58 +0100

    Ajout de la partie DBUS pour la com série et la partie servo

--- beaglebone/logiciel_beaglebone/servo_dbus/main.py
+++ beaglebone/logiciel_beaglebone/servo_dbus/main.py
@@ -1,32 +1,57 @@
+#!/usr/bin/python
+
 import dbus
 import dbus.service
 import dbus.mainloop.glib
 
 import gobject
+import json
+from turret import *
 
 class Service(dbus.service.Object):
-   def __init__(self, message):
-      self._message = message
+    def __init__(self, message):
+        self._message = message
+    
+    def run(self):
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        bus_name = dbus.service.BusName("com.turret.test", dbus.SessionBus())
+        dbus.service.Object.__init__(self, bus_name, "/com/turret/test")
+        self.t = Turret()
+        self._loop = gobject.MainLoop()
+        print "Service running..."
+        self._loop.run()
+        print "Service stopped"
 
-   def run(self):
-      dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-      bus_name = dbus.service.BusName("com.turret.service", dbus.SessionBus())
-      dbus.service.Object.__init__(self, bus_name, "/com/turret/service")
-
-      self._loop = gobject.MainLoop()
-      print "Service running..."
-      self._loop.run()
-      print "Service stopped"
-
-   @dbus.service.method("com.turret.service.Message", in_signature='', out_signature='s')
-   def get_message(self):
-      print "  sending message"
-      return self._message
-
-   @dbus.service.method("com.turret.service.Quit", in_signature='', out_signature='')
-   def quit(self):
-      print "  shutting down"
-      self._loop.quit()
+    @dbus.service.method("com.turret.test", in_signature='s', out_signature='')
+    def draw(self, shape):
+        self.t.draw(shape)
+        
+    @dbus.service.method("com.turret.test", in_signature='ss', out_signature='')
+    def moveTo(self, x, y):
+        self.t.shape.moveTo(int(x), int(y))
+        
+    @dbus.service.method("com.turret.test", in_signature='', out_signature='')
+    def reset(self):
+        self.t.shape.moveTo(90, 90)
+    
+    @dbus.service.method("com.turret.test", in_signature='s', out_signature='')
+    def addAngleHorizontal(self, value):
+        self.t.addAngleHorizontal(int(value))
+        
+    @dbus.service.method("com.turret.test", in_signature='s', out_signature='')
+    def addAngleVertical(self, value):
+        self.t.addAngleVertical(int(value))
+    
+    @dbus.service.method("com.turret.test", in_signature='s', out_signature='')
+    def led(self, value):
+        if value == "on":
+            self.t.turnOnLaser()
+        elif value == "off":
+            self.t.turnOffLaser()
+        
+    @dbus.service.method("com.turret.test", in_signature='', out_signature='')
+    def quit(self):
+        self._loop.quit()
 
 if __name__ == "__main__":
-   Service("This is the service").run()
+   Service("Demarrage de la tourelle").run()
